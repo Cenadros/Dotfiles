72f42cbbfd6e39266d74d6ca266b478089ff77d2
Alex Sánchez <alejandro@runroom.com>
2022-04-21 11:33:23 +0200

    added nvm

--- install.bash
+++ install.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
 git clone --recurse-submodules https://github.com/belak/prezto-contrib "${HOME}/.zprezto/contrib"
+git clone https://github.com/nvm-sh/nvm.git "${HOME}/.nvm"
+pushd ${HOME}/.nvm
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+popd
 
 bash symlinks.bash
--- update.bash
+++ update.bash
@@ -6,3 +6,7 @@ git pull origin main
 pushd ${HOME}/.zprezto
 git pull && git submodule update --init --recursive
 popd
+
+pushd ${HOME}/.nvm
+git fetch --tags origin && git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+popd
